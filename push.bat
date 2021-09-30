aae012c8a98751bd449aa00a3b871e07883d4fef
hunsou <hunsou@msn.com>
2021-09-30 12:25:38 +0800

    2021-09-30 12:25:38

---
+++ push.bat
@@ -0,0 +1,10 @@
+@echo off
+::git rm -r --cached .
+git add .
+git commit -m "%date:~6,4%-%date:~0,2%-%date:~3,2% %time:~0,8%"
+::git tag -a v1.1.7 -m ""
+rem git push origin master --tags
+:gitpush
+git push origin master
+IF ERRORLEVEL 1 goto gitpush
+if %errorlevel%==1 goto gitpush
